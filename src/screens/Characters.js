950475ad30f5bfcabce9f2cb92a7ca5b21f3ae61
MUNSCH Thomas <munsch.thomas@gmail.com>
2022-06-14 22:40:44 +0200

    modified Contact.css to display correctly Characters.js

--- src/screens/Characters.js
+++ src/screens/Characters.js
@@ -1,32 +1,32 @@
-import { useState, useEffect } from "react"
-import axios from "axios"
+import { useState, useEffect } from "react";
+import axios from "axios";
 // import "./Contact.css"
 
 const Characters = () => {
-	const [characters, setCharacters] = useState([])
-	useEffect(() => {
-		console.log("poulet 02:")
-		// fetch("http://hp-api.herokuapp.com/api/characters")
-		// 	.then((res) => res.json())
-		// 	.then((res) => console.log(res))
-		axios
-			.get("http://hp-api.herokuapp.com/api/characters")
-			.then((res) => console.log(res.data) || setCharacters(res.data))
-	}, [])
-	return (
-		<div className="yolo">
-			{console.log("poulet 01:")}poulet character
-			{characters.map(
-				(character) =>
-					console.log(character.image) || (
-						<div>
-							<img src={character.image} alt={character.name} />
-							<p>{character.name}</p>
-						</div>
-					)
-			)}
-		</div>
-	)
-}
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    console.log("poulet 02:");
+    // fetch("http://hp-api.herokuapp.com/api/characters")
+    // 	.then((res) => res.json())
+    // 	.then((res) => console.log(res))
+    axios
+      .get("http://hp-api.herokuapp.com/api/characters")
+      .then((res) => console.log(res.data) || setCharacters(res.data));
+  }, []);
+  return (
+    <div className="contact__container">
+      {console.log("poulet 01:")}poulet character
+      {characters.map(
+        (character) =>
+          console.log(character.image) || (
+            <div>
+              <img src={character.image} alt={character.name} />
+              <p>{character.name}</p>
+            </div>
+          )
+      )}
+    </div>
+  );
+};
 
-export default Characters
+export default Characters;
